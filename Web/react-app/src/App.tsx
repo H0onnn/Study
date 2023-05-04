5bd1ca08980107bbf76b1c5e72074b402e90f656
H0onnn <116232939+H0onnn@users.noreply.github.com>
2023-05-04 19:37:38 +0900

    react state와 fomr태그의 연동

--- Web/react-app/src/App.tsx
+++ Web/react-app/src/App.tsx
@@ -29,6 +29,9 @@ function getCurrentTime() {
   return `${hours}:${minutes}:${seconds}`;
 }
 
+// 1. state가 arr or obj일 경우 값을 변경할 때 복제본을 생성하여 변경
+// 2. for나 map의 활용할 땐 key값을 제공
+// 3. state가 배열일 때 값이 없다면 추론을 사용할 수 없기에 제네릭을 명시적으로 입력
 function Counter2() {
   const [times, setTimes] = useState<string[]>([]);
   console.log("component - times", times);
@@ -50,6 +53,27 @@ function Counter2() {
   );
 }
 
+// input과 같은 form 태그와 state를 연동해서 react에서 다루기
+function Counter3() {
+  const [count, setCount] = useState<number>(0);
+  const [step, setStep] = useState<number>(10);
+  return (
+    <>
+      <h1>Counter3</h1>
+      <input
+        type="number"
+        value={step}
+        onChange={(e) => {
+          console.log(e.target.value);
+          setStep(+e.target.value);
+        }}
+      />
+      <button onClick={() => setCount(count + step)}>+</button>
+      {count}
+    </>
+  );
+}
+
 function App() {
   return (
     <div>
@@ -57,6 +81,7 @@ function App() {
       <Counter title="강아지 카운터"></Counter>
       <Counter title="고양이 카운터"></Counter>
       <Counter2 />
+      <Counter3 />
     </div>
   );
 }
